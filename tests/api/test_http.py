e4004f3879409ac9c610d7fcb48ed715d7671e2b
Viacheslav Pavlenko <pavlenko.vyacheslav@gmail.com>
2023-05-04 22:47:20 +0300

    The tests for requests has been improved

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -5,23 +5,23 @@ import requests
 @pytest.mark.http
 def test_first_request():
     r = requests.get('https://api.github.com/zen')
-    print("==================================================================")
-    print(f"Status code is: {r.status_code}")
-    print("==================================================================")
     print(f"Response is: {r.text}")
+    # print(f"Status code is: {r.status_code}")
 
 
 @pytest.mark.http
 def test_second_request():
-    r = requests.get('https://api.github.com/users/pavlenkovv')
-    print(f"Response Body is: {r.json()}")
-    print("==================================================================")
-    print(f"Response status code is: {r.status_code}")
-    print("==================================================================")
-    print(f"Response Headers is: {r.headers}")
+    r = requests.get('https://api.github.com/users/defunkt')
+    body = r.json()
+    headers = r.headers
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
 
 
 @pytest.mark.http
-def test_third_request():
-    r = requests.get('https://api.github.com/events')
-    print(f"Response is: {r.text}")
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/sergii_butenko')
+
+    assert r.status_code == 404
