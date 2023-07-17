1b937f2b713660421e2e1def3c8b994d82d4a517
Viacheslav Pavlenko <pavlenko.vyacheslav@gmail.com>
2023-07-17 18:08:17 +0300

    added test

--- tests/ui/test_ui_page_object.py
+++ tests/ui/test_ui_page_object.py
@@ -1,5 +1,7 @@
-from modules.ui.page_objects.sign_in_page import SignInPage
+import time
 import pytest
+from selenium.webdriver.common.by import By
+from modules.ui.page_objects.sign_in_page import SignInPage
 
 
 @pytest.mark.ui
@@ -20,5 +22,18 @@ def test_check_wrong_password_link_page_object():
     sign_in_page.click_forgot_password_link()
 
     assert sign_in_page.check_title("Forgot your password? · GitHub")
+
+    sign_in_page.close()
+
+
+@pytest.mark.ui
+def test_click_on_the_cat_page_object():
+    sign_in_page = SignInPage()
+    sign_in_page.go_to()
+    sign_in_page.driver.find_element(
+        By.CSS_SELECTOR, ".octicon.octicon-mark-github"
+    ).click()
+    time.sleep(2)
+    assert sign_in_page.check_title("GitHub: Let’s build from here · GitHub")
 
     sign_in_page.close()
